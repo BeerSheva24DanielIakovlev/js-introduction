c091dc176d87d43c900c9610132dea49e146f349
Daniel <gromdan90@gmail.com>
2025-01-26 15:05:09 +0200

    introspecting

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,10 +1,25 @@
 import { describe, it, expect } from "vitest";
 
-describe("sorting array", () => {
-    const array = [10, 1000, -10, 30, 60, 50];
+describe("array introspecting", () => {
+    const array = [10, 1000, -10, 30, 60];
 
-    it("native sorting", () => {
-        const expected = [-10, 10, 30, 60, 1000];
-        expect(array.sort((a, b) => a - b)).toEqual(expected);
+    it("includes", () => {
+        const objects = [
+            {x:4},
+            {x:5}
+        ];
+        expect(array.includes(1000)).toBeTruthy();
+        expect(array.includes(200)).toBeFalsy();
+        expect(array.includes({x:4})).toBeFalsy();
+    });
+
+    it("testing for array ob objects", () => {
+        expect(objects.find(ovj.x === 4)).toBeTruthy();
+        expect(objects.find(ovj.x === 1)).toBeFalsy();
+    });
+
+    it("testing for all elements match acondition", () => {
+        expect(array.every(num => num % 2 === 0)).toBeTruthy();
+        expect(array.some(num => num % 2 !== 0)).toBeFalsy();
     });
 });
