5639e7a7967be3408337d63489ba5baf2b0ec7f7
Daniel <gromdan90@gmail.com>
2025-01-26 14:29:41 +0200

    sorting array

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,25 +1,10 @@
 import { describe, it, expect } from "vitest";
 
-describe("iterating elements of array", () => {
-    const ar = [1, 2, 3, 4];
+describe("sorting array", () => {
+    const array = [10, 1000, -10, 30, 60, 50];
 
-    it("printing out of elements usinf for-in", ()=> {
-        for(let i in ar) {
-            console.log(ar[i]);
-        }
-    });
-
-    it("printing out of elements usinf for-of", ()=> {
-        for(let num of ar) {
-            console.log(ar[num]);
-        }
-    });
-
-    it("printing out elements  using forEach method", () => {
-        ar.forEach(e => console.log(e));
-    });
-
-    it("printing out index and element from array", () => {
-        ar.forEach((e, i) => console.log(`index: ${i}; element: ${e}`))
+    it("native sorting", () => {
+        const expected = [-10, 10, 30, 60, 1000];
+        expect(array.sort((a, b) => a - b)).toEqual(expected);
     });
 });
