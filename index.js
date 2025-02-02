61b45c1261998d502d1f21fed016c76be8601cc3
Daniel <gromdan90@gmail.com>
2025-02-02 10:48:26 +0200

    selia

---
+++ index.js
@@ -0,0 +1,32 @@
+// // 'use strict'
+// // a = 1;
+// fun1();
+// // fun2();
+
+// // fun3();
+// function fun1() {
+//     // function fun2() {
+//     //     console.log("fun2")
+//     // }
+//     a = 10;
+//     fun2();
+// };
+
+// function fun2() {
+//     while(a < 3) {
+//         console.log(a++);
+//     };
+// };
+
+// function fun3() {
+//     console.log(`a=${a}`);
+// };
+
+// console.log(a);
+
+function sleep(timeout) {
+    let running = true;
+    while (running) {
+        setTimeout(() => running = false, timeout);
+    };
+};
